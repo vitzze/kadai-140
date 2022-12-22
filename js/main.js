b0a3df55c0082616bcba1195ca6d676588cd028d
vitzze <118044884+vitzze@users.noreply.github.com>
2022-12-22 11:38:12 +0900

    Modify js

--- js/main.js
+++ js/main.js
@@ -27,6 +27,22 @@
     });
   });
 
+// -------------------
+// ページ内リンクへの移動
+// -------------------
+
+$('a[href^="#"]').click(function(){
+  // リンクを取得
+  let href= $(this).attr("href");
+  // ジャンプ先のid名をセット
+  let target = $(href == "#" || href == "" ? 'html' : href);
+  // トップからジャンプ先の要素までの距離を取得
+  let position = target.offset().top;
+  // animateでスムーススクロールを行う
+  // 600はスクロール速度で単位はミリ秒
+  $("html, body").animate({scrollTop:position}, 600, "swing");
+  return false;
+});
 
 
 // -------------------
